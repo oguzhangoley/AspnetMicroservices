d53c56260e2c172546af38c11251d6f223d93a94
oguzhan <oguzhanngoley@gmail.com>
2023-11-20 18:01:41 +0300

    grpc service dockerized and tested

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DisCountProtoService.DisCountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DisCountProtoService.DisCountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
