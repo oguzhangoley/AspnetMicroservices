4524790179d6be6a5818c711a9cfbc35ffbd76c4
oguzhan <oguzhanngoley@gmail.com>
2023-11-22 12:28:56 +0300

    cqrs layers added

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IMapper mapper, ILogger<DeleteOrderCommandHandler> logger, IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToDelete == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+            await _orderRepository.DeleteAsync(orderToDelete);
+
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+        }
+    }
+}
